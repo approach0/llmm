5c36e107566717d4ab8a50deacc0b4b5f103ced0
t-k- <clock126@126.com>
2023-07-11 02:09:26 -0400

    added deepspeed tests.

---
+++ ds_test.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+import torch
+
+import deepspeed
+from transformers.deepspeed import HfDeepSpeedConfig
+
+from transformers import LlamaConfig
+from transformers import LlamaTokenizer
+from transformers import LlamaForCausalLM
+
+model_path = sys.argv[-1]
+local_rank = int(os.getenv("LOCAL_RANK", "0"))
+world_size = int(os.getenv("WORLD_SIZE", "1"))
+
+with open('ds_config_zero3.json') as fh:
+    ds_config = json.load(fh)
+ds_config['train_batch_size'] = world_size * 16
+# this has to be run before loading the model.from_pretrained()
+ds_config_hf = HfDeepSpeedConfig(ds_config)
+
+torch.cuda.set_device(local_rank)
+model_path = os.path.expanduser(model_path)
+
+tokenizer = LlamaTokenizer.from_pretrained(model_path)
+model = LlamaForCausalLM.from_pretrained(model_path)
+
+ds_engine = deepspeed.initialize(model=model, config=ds_config)[0]
+ds_engine.module.eval() # for inference
+
+text_in = 'My name is Mariama, my favorite '
+inputs = tokenizer.encode(text_in, return_tensors="pt").to(device=local_rank)
+with torch.no_grad():
+    outputs = ds_engine.module.generate(inputs,
+        synced_gpus=True, max_new_tokens=32, do_sample=True)
+text_out = tokenizer.decode(outputs[0], skip_special_tokens=True)
+if local_rank == 0:
+    print(f"{text_in} {text_out}")
---
+++ ds_test.sh
@@ -0,0 +1,13 @@
+export NCCL_BLOCKING_WAIT=1  # Set this variable to use the NCCL backend
+export NCCL_IB_DISABLE=1
+export NCCL_DEBUG=INFO
+export NCCL_P2P_DISABLE=1 # direct access between GPUs? using NVLink or PCI.
+# See https://github.com/NVIDIA/nccl/issues/631
+
+#export TORCH_DISTRIBUTED_DEBUG=DETAIL
+export TORCH_DISTRIBUTED_DEBUG=OFF
+
+#deepspeed --num_gpus 1 ds_test.py ~/llama-models/7B-hgf-new/
+#deepspeed --num_gpus 1 ds_test.py ~/llama-models/13B-hgf-new/
+#deepspeed --num_gpus 2 ds_test.py ~/llama-models/30B-hgf/
+deepspeed --num_gpus 3 ds_test.py ~/llama-models/65B-hgf-new/
