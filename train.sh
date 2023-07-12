a33ba959d00bff5268ea57ab1bb68985e2aaf241
t-k- <clock126@126.com>
2023-07-12 02:01:31 -0400

    lower batch size.

--- train.sh
+++ train.sh
@@ -19,8 +19,8 @@ deepspeed \
     --learning_rate 2e-5 \
     --bf16_full_eval True \
     --logging_steps 2 \
-    --warmup_steps 3000 \
+    --warmup_steps 3 \
     --report_to "tensorboard" \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --deepspeed ds_config_zero3.json \
     --model_name_or_path ~/llama-models/7B-hgf-new/
