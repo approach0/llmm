0620bd16326a011a109e91e70f351df47b5257d1
t-k- <clock126@126.com>
2023-07-18 18:57:53 -0400

    added tools/test_chatgpt.py

---
+++ tools/test_chatgpt.py
@@ -0,0 +1,91 @@
+import os
+import time
+import requests
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+EXAMPLE_SEPARATOR_TOKEN = '\n\n'
+
+
+class OAI_API():
+    def __init__(self):
+        self.endpoint = "https://corby.openai.azure.com/"
+        self.name = "corby"
+        self.oaikey = os.environ.get('OAIKey')
+        self.engine = "gpt-35-turbo"
+        self.deployments = {
+            "text-davinci-002" : "text-davinci-002",
+            "text-davinci-003" : "text-davinci-003",
+            "gpt-35-turbo" : "gpt-35-turbo"
+        }
+        self.api_version = "2022-06-01-preview"
+
+    def get_completion(self, prompt, num_tokens = 5000, num_samples = 1,
+                    stop = EXAMPLE_SEPARATOR_TOKEN, include_log_probs = 0,
+                    temperature = 0.0, get_probs_of = []):
+        get_probs_of = set(get_probs_of)
+        request_url = (f"https://{self.name}.openai.azure.com/openai/deployments/" +
+            f"{self.deployments[self.engine]}/completions")
+
+        headers = {
+            'api-key': self.oaikey,
+        }
+        params = {
+            'api-version': self.api_version,
+        }
+        tries = 0
+        max_tries = 5
+        output = {}
+        sleep_time = 1
+        while True:
+            try:
+                json_data = {
+                    'prompt': prompt,
+                    'max_tokens': num_tokens,
+                    'temperature' : temperature,
+                    'stop' : stop,
+                    'n' : num_samples,
+                }
+                print(request_url)
+                print(headers)
+                print(json_data)
+                response = requests.post(request_url, params=params, headers=headers, json=json_data)
+                output = response.json()
+                valid_output = output.copy()
+                valid_output["choices"] = [x['text'].strip() for x in output['choices']]
+                if include_log_probs > 0:
+                    valid_output["top_logprobs"] = [x['logprobs'] for x in output['choices']]
+                if get_probs_of:
+                    valid_output["token_log_prob"] = {}
+                    for token, prob in zip(
+                        output['choices'][0]["logprobs"]["tokens"],
+                        output['choices'][0]["logprobs"]["token_logprobs"]):
+                        if token in get_probs_of:
+                            valid_output["token_log_prob"][token] = prob
+                sleep_time = 1
+            except Exception as e:
+                print(output)
+                tries += 1
+                if tries > max_tries:
+                    return output
+                if "error" in output:
+                    valid_output = None
+                    if ("code" in output and "content_filter" in output["code"]) or "filter" in output["error"].get("message", ""):
+                        return output
+                    if "maximum context length" in output["error"].get("message", ""):
+                        valid_output = -1
+                        split_prompt = prompt.split(EXAMPLE_SEPARATOR_TOKEN)
+                        new_prompt = [split_prompt[0]] + split_prompt[2:]
+                        prompt = EXAMPLE_SEPARATOR_TOKEN.join(new_prompt)
+                        print(f"Error: {output['error']['message']}, shortening prompt")
+                sleep_time *= 2
+                time.sleep(sleep_time)
+                continue
+            break
+        return valid_output
+
+
+if __name__ == '__main__':
+    api = OAI_API()
+    out = api.get_completion('Hello! How many languages do you speak?')
+    print(out['choices'][0])
