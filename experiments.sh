0491b858397a13d7fecbfc3a0f828bce8dd86091
t-k- <clock126@126.com>
2023-07-21 22:01:24 -0400

    update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,31 @@
-#export CUDA_VISIBLE_DEVICES=2
-#python tools/multi-hops.py --mode=cot --pass_name=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+SETUP=${1}
 
-export CUDA_VISIBLE_DEVICES=2
-python tools/multi-hops.py --mode=ia --pass_name=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+if [[ -z $SETUP ]]; then
+    echo "please specify SETUP."
+fi
+
+case $SETUP in
+    cot-vicuna-7b)
+    export CUDA_VISIBLE_DEVICES=2
+    python tools/multi-hops.py \
+        --logname $SETUP --mode=cot --pass_name=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+    ;;
+
+    ia-vicuna-7b)
+    export CUDA_VISIBLE_DEVICES=2
+    python tools/multi-hops.py \
+        --mode=ia --pass_name=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+    ;;
+
+    cot-vicuna-13b)
+    export CUDA_VISIBLE_DEVICES=2
+    python tools/multi-hops.py \
+        --logname $SETUP --mode=cot --pass_name=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
+    ;;
+
+    ia-vicuna-30b)
+    export CUDA_VISIBLE_DEVICES=2
+    python tools/multi-hops.py \
+        --mode=ia --pass_name=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
+    ;;
+esac
