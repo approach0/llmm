a00e5ae733857d93182923d113876a1c3230db5d
t-k- <clock126@126.com>
2023-07-23 01:42:15 -0400

    made ds_test* working again.

--- ds_test.py
+++ ds_test.py
@@ -10,17 +10,17 @@ from transformers import LlamaConfig
 from transformers import LlamaTokenizer
 from transformers import LlamaForCausalLM
 
+tokenizer_path = sys.argv[-2]
 model_path = sys.argv[-1]
+
 local_rank = int(os.getenv("LOCAL_RANK", "0"))
 world_size = int(os.getenv("WORLD_SIZE", "1"))
 
 with open('ds_config_zero3.json') as fh:
     ds_config = json.load(fh)
-ds_config['train_batch_size'] = (
-    ds_config['train_micro_batch_size_per_gpu'] *
-    ds_config['gradient_accumulation_steps'] *
-    world_size
-)
+ds_config['train_batch_size'] = world_size
+del ds_config['gradient_accumulation_steps']
+del ds_config['train_micro_batch_size_per_gpu']
 
 # this has to be run before loading the model.from_pretrained()
 ds_config_hf = HfDeepSpeedConfig(ds_config)
@@ -28,19 +28,32 @@ ds_config_hf = HfDeepSpeedConfig(ds_config)
 torch.cuda.set_device(local_rank)
 model_path = os.path.expanduser(model_path)
 
-tokenizer = LlamaTokenizer.from_pretrained(model_path)
+tokenizer = LlamaTokenizer.from_pretrained(tokenizer_path)
 model = LlamaForCausalLM.from_pretrained(model_path,
     torch_dtype=torch.float16)
 
 ds_engine = deepspeed.initialize(model=model, config=ds_config)[0]
-ds_engine.module.eval() # for inference
+model = ds_engine.module
+model.eval() # for inference
 
-text_in = 'My name is Mariama, my favorite '
-text_in = "When I was 6 my sister was half my age. Now I'm 70, my sister's age is "
-inputs = tokenizer.encode(text_in, return_tensors="pt").to(device=local_rank)
+text_in = '''
+Below is an instruction that describes a task, paired with an input that provides further context.
+Write a response that appropriately completes the request.
+
+### Instruction:
+Give three tips for staying healthy.
+
+### Input:
+
+### Response:
+'''
+inputs = tokenizer.encode(text_in, return_tensors="pt")
+inputs = inputs.to(device=local_rank)
 with torch.no_grad():
-    outputs = ds_engine.module.generate(inputs,
-        synced_gpus=True, max_new_tokens=32, do_sample=True)
-text_out = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    outputs = model.generate(inputs,
+        synced_gpus=True, max_new_tokens=128, do_sample=True)
+text_out = tokenizer.decode(outputs[0],
+    skip_special_tokens=True)
+
 if local_rank == 0:
     print(f"{text_in} {text_out}")
