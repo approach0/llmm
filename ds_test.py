b35e4a9e1a8437c96b0579acde65664315fc12fc
t-k- <clock126@126.com>
2023-07-23 03:45:09 -0400

    get flash attention v2 working.

--- ds_test.py
+++ ds_test.py
@@ -12,6 +12,15 @@ from transformers import LlamaForCausalLM
 
 #from auto_gptq import AutoGPTQForCausalLM
 
+import sys
+sys.path.insert(0, '../FastChat')
+from flash_attn_monkey_patch import (
+    replace_llama_attn_with_flash_attn,
+)
+use_flash_att2 = True
+if use_flash_att2:
+    replace_llama_attn_with_flash_attn()
+
 tokenizer_path = sys.argv[-2]
 model_path = sys.argv[-1]
 
@@ -39,7 +48,7 @@ ds_engine = deepspeed.initialize(model=model, config=ds_config)[0]
 model = ds_engine.module
 model.eval() # for inference
 
-text_in = '''
+default_prompt = '''
 Below is an instruction that describes a task, paired with an input that provides further context.
 Write a response that appropriately completes the request.
 
@@ -50,15 +59,32 @@ Give three tips for staying healthy.
 
 ### Response:
 '''
-#inputs = tokenizer(text_in, return_tensors="pt")
-inputs = tokenizer.encode(text_in, return_tensors="pt")
-inputs = inputs.to(device=local_rank)
-with torch.no_grad():
-    outputs = model.generate(inputs,
-        synced_gpus=True, max_new_tokens=128, do_sample=True)
-    #outputs = model.generate(**inputs)
-text_out = tokenizer.decode(outputs[0],
-    skip_special_tokens=True)
+def inference(prompt=default_prompt):
+    print('rank', local_rank, end='\n\n')
+    #inputs = tokenizer(prompt, return_tensors="pt")
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
+    inputs = inputs.to(device=local_rank)
+    use_cache = False if use_flash_att2 else True
+    with torch.no_grad():
+        outputs = model.generate(inputs, use_cache=use_cache,
+            synced_gpus=True, max_new_tokens=128,
+            do_sample=True)
+        #outputs = model.generate(**inputs)
+    text_out = tokenizer.decode(outputs[0],
+        skip_special_tokens=True)
 
-if local_rank == 0:
-    print(f"{text_in} {text_out}")
+    if local_rank == 0:
+        print(f"{prompt} {text_out}")
+    return text_out
+
+if local_rank != 0:
+    while True:
+        inference()
+else:
+    import gradio as gr
+    iface = gr.Interface(
+        fn=inference, inputs="text", outputs="text")
+    # Enabling the queue for inference times > 60 seconds:
+    iface.queue().launch(server_port=8922,
+        debug=True, share=True, inline=False
+    )
