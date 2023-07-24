10f72ddbe2f73cf1328dbafb8ba2e9264c4ab78d
t-k- <clock126@126.com>
2023-07-24 16:44:43 -0400

    update tools/inspect_output.py

--- tools/inspect_output.py
+++ tools/inspect_output.py
@@ -11,7 +11,7 @@ def get_topic_stats(logdir):
         agent_answer = j['agent_answer']
         ground_truth = j['ground_truth']
         is_equiv = j['is_equiv']
-        print(agent_answer, ground_truth, is_equiv)
+        print(logpath, '\t', agent_answer, '\t', ground_truth, '\t', is_equiv)
         if is_equiv:
             correct_cnt += 1
         total_cnt += 1
@@ -20,15 +20,28 @@ def get_topic_stats(logdir):
     print(f'Accuracy: {correct_cnt} / {total_cnt} = {accuracy_percentage:.2f}%')
 
 
-def output_html(file_path):
+def output_html(logpath):
     import sys
     sys.path.insert(0, './pya0')
+    from pya0.visualize import output_html as output
+    with open(logpath, 'r') as fh:
+        j = json.load(fh)
+        results = [f'<b>{key}</b>: {val}' for key, val in j.items()]
+
+        def html(fh, query, hit, page, idx):
+            hit = hit.replace('\n', '<br/>')
+            fh.write(f'<p>{hit}</p>\n\n')
+
+        logdir = os.path.dirname(logpath)
+        logbase = os.path.basename(logpath)
+        output(logdir, logbase, '_', j['problem'], results,
+            None, False, 100, html, create_parent_dir=False)
 
 
 if __name__ == '__main__':
     import fire
     os.environ["PAGER"] = 'cat'
     fire.Fire({
-        'get_topic_stats': get_topic_stats,
+        'get_stats': get_topic_stats,
         'output_html': output_html
     })
