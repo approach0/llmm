13a1406c9c55203e905e04d2d2accd2f1c9edf0f
t-k- <clock126@126.com>
2023-07-26 15:17:31 -0400

    added tools/data-gen-merged-pairs.py

---
+++ tools/data-gen-merged-pairs.py
@@ -0,0 +1,39 @@
+import os
+import json
+import random
+
+
+def merge_pairs(*json_paths, output_file='output/finetune-pairs.json',
+    shuffle=True, seed=72):
+
+    output = []
+    for json_path in json_paths:
+        print(json_path)
+        with open(json_path, 'r') as fh:
+            j = json.load(fh)
+            output += j
+
+    if shuffle:
+        random.seed(seed)
+        random.shuffle(output)
+
+    print(f'Saving {len(output)} pairs ...')
+    with open(output_file, 'w', encoding='utf8', errors='replace') as fh:
+        json.dump(output, fh, indent=2, ensure_ascii=False)
+
+
+def sanity_check(json_path):
+    with open(json_path, 'r') as fh:
+        j = json.load(fh)
+    for i, item in enumerate(j):
+        if 'boxed' in item['instruction']:
+            print(i)
+
+
+if __name__ == '__main__':
+    import fire
+    os.environ["PAGER"] = 'cat'
+    fire.Fire({
+        'merge': merge_pairs,
+        'check': sanity_check
+    })
