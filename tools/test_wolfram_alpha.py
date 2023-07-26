5e5f1e1e4b04ad4943ab9f1bbbd0a110c0441bfc
t-k- <clock126@126.com>
2023-07-26 16:57:51 -0400

    added tools/test_wolfram_alpha.py

---
+++ tools/test_wolfram_alpha.py
@@ -0,0 +1,18 @@
+import os
+import wolframalpha
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+wolfram_alpha_appid = os.environ.get('WolframKey')
+
+def WolframAlphaCalculator(input_query: str):
+    wolfram_client = wolframalpha.Client(wolfram_alpha_appid)
+    res = wolfram_client.query(input_query)
+    assumption = next(res.pods).text
+    answer = next(res.results).text
+    return f"Assumption: {assumption} \nAnswer: {answer}"
+
+
+if __name__ == '__main__':
+    print(WolframAlphaCalculator('what is 7 mod 5?'))
