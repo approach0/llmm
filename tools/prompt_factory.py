70d74009ed688ea9d5c48e3d0041cd0869dd82f1
t-k- <clock126@126.com>
2023-07-28 19:43:06 -0400

    added multi-hop prompt template.

--- tools/prompt_factory.py
+++ tools/prompt_factory.py
@@ -163,6 +163,133 @@ Let's think step by step, and derive the final answer.
     return prompt
 
 
+#########################
+####### version 3 #######
+#########################
+
+
+def multihop1(Q):
+    # example is extracted from MATH precalculus/1183.json 
+    prompt = r'''Below is an Instruction section that describes a task, paired with an Input section that provides further context.
+Write in the Response section that appropriately completes the request.
+
+### Instruction:
+Answer a math question in the input. You can invoke a math-aware search engine API (SEARCH[*]) or a computation API (COMPUTE[*]) as you like, and I will insert the returned API results for you right after SEARCH or COMPUTE calls.
+
+At the end, indicate your final answer in boxed LaTeX. For example, if you think the final answer is \sqrt{3}, write it as \boxed{\sqrt{3}} (in boxed LaTeX) at the very end of your output.
+
+Now, let me walk you through just one example first.
+
+--- PROBLEM BEGIN ---
+Find the number of solutions in the interval $[0,2\pi]$ to equation $\tan x + \sec x = 2 \cos x.$
+--- PROBLEM END   ---
+
+I feel uncertain about his problem, let me search the key formula first...
+
+SEARCH[$\tan x + \sec x = 2 \cos x.$]
+
+Here are the results:
+--- RESULTS BEGIN ---
+I tried changing it all to sin and cos
+
+$$
+\frac{\sin x}{\cos x} + \frac{1}{\cos x} = 2\cos x
+$$
+
+then I made it to one fraction
+
+$$
+\frac{\sin x + 1}{\cos x} = 2 \cos x
+$$
+
+Then I don't know where to go from there. Please help!
+--- NEXT RESULT ---
+That gives you: $\sin x= 2\cos^2x-1 = \cos(2x).$
+It should be easy from here.
+--- RESULTS END   ---
+
+Okay, the second results look very relevant. Now what left to be solved is
+
+$$
+\sin x = \cos(2x)
+$$
+
+Let me see if this equation has been solved before...
+
+SEARCH[$\sin x = \cos(2x)$]
+
+Here are the results:
+--- RESULTS BEGIN ---
+Hint: Use the fact that $\cos(2x) = 1 - 2\sin^2x$.
+Then you will have a quadratic for $\sin x$.
+--- NEXT RESULT ---
+I'm guessing that you mean either for the integral to run from 0 to π/2, or from −π/2 to 0.
+--- RESULTS END   ---
+
+Okay, the 2nd result is not relevant, but the first one is a very good idea. If we plug into the formula using $\cos(2x) = 1 - 2\sin^2x$, then
+
+$$
+\sin x = 1 - 2\sin^2x
+$$
+
+if we let $\sin x = y$, then above is essentially
+
+$$
+y = 1 - 2 y^2
+$$
+
+this easy equation should be solvable by computation API...
+
+COMPUTE[solve $y = 1 - 2 y^2$ for y]
+
+Here are the results:
+--- RESULTS BEGIN ---
+[-1, 1/2]
+--- RESULTS END ---
+
+Okay, that means $\sin(x)$ is either $-1$ or $1/2$. Let me invoke computation API again.
+
+For $-1$ case...
+
+COMPUTE[solve $\sin x = -1$ for x]
+
+Here are the results:
+--- RESULTS BEGIN ---
+[-pi/2, 3*pi/2]
+--- RESULTS END ---
+
+Okay, for the other $1/2$ case...
+
+COMPUTE[solve $\sin x = 1/2$ for x]
+
+Here are the results:
+--- RESULTS BEGIN ---
+[pi/6, 5*pi/6]
+--- RESULTS END ---
+
+Okay, by collecting these results, and knowning that they have to be in the interval $[0,2\pi]$, we get
+
+[3*pi/2, pi/6, 5*pi/6]
+
+So the number of solutions is $\boxed{2}$.
+
+You get the idea? Now it is your turn!
+'''
+
+
+    prompt += '''
+### Input:
+--- PROBLEM BEGIN ---
+{}
+--- PROBLEM END   ---
+
+'''.format(Q)
+
+    prompt += r'''
+### Response:
+'''
+    return prompt
+
+
 if __name__ == '__main__':
-    print(cot1('Why are universal models difficult to serve at web search scale?'))
-    print(ia1('Why are universal models difficult to serve at web search scale?', 'universal models are more general and they require more compute resources'))
+    print(multihop1(r'Let $x$ be a real number such that $\sec x - \tan x = 2$. Find $\sec x + \tan x.$'))
--- tools/test_sympy.py
+++ tools/test_sympy.py
@@ -4,5 +4,7 @@ from sympy import solve
 
 
 if __name__ == '__main__':
-    res = solve(parse_latex(r'x=1-2x^2'), symbols('x'))
+    #res = solve(parse_latex(r'y = 1 - 2 y^2'), symbols('y'))
+    #res = solve(parse_latex(r'\sin x = -1'), symbols('x'))
+    res = solve(parse_latex(r'\sin x = 1/2'), symbols('x'))
     print(res)
