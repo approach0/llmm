fda04013a2765ba5e1da04b6e581684906f7734a
t-k- <clock126@126.com>
2023-07-31 18:43:42 -0400

    add err msg to test_sympy

--- tools/test_sympy.py
+++ tools/test_sympy.py
@@ -9,37 +9,44 @@ from sympy import latex
 
 def compute(mode, latex_expr):
     latex_expr = latex_expr.strip().strip('$')
-    if mode == 'calculate':
-        res = parse_latex(latex_expr).evalf(3, subs={
-            'pi': pi
-        })
-        return str(latex(res))
+    try:
+        if mode == 'calculate':
+            res = parse_latex(latex_expr).evalf(3, subs={
+                'pi': pi
+            })
+            return str(latex(res))
 
-    elif mode == 'simplify':
-        res = simplify(parse_latex(latex_expr))
-        return str(latex(res))
+        elif mode == 'simplify':
+            res = simplify(parse_latex(latex_expr))
+            return str(latex(res))
 
-    elif mode.startswith('solve'):
-        var = mode.split('solve ')[-1].strip()
-        res = solve(parse_latex(latex_expr), symbols(var))
-        return str(latex(res))
+        elif mode.startswith('solve'):
+            var = mode.split('solve ')[-1].strip()
+            res = solve(parse_latex(latex_expr), symbols(var))
+            return str(latex(res))
 
-    else:
-        return 'Wrong input format, try again!'
+        else:
+            return 'Error: Wrong input format, try again!'
+
+    except Exception as err:
+        return 'Error: ' + str(err)
 
 
 if __name__ == '__main__':
-    res = compute('solve x', r'\sin x = 1/2')
-    print(res)
-    res = compute('simplify', r'x + y - 3 + 2 + x')
-    print(res)
-    res = compute('calculate', r'\pi + \sin(\pi / 2)')
-    print(res)
-    res = compute('foo', 'bar')
-    print(res)
-    res = compute("solve y", "$y = 1 - 2 y^2$")
-    print(res)
-    res = compute("solve x", "$\\sin x = -1$")
-    print(res)
-    res = compute("solve x", "$\\sin x = 1/2$")
+    #res = compute('solve x', r'\sin x = 1/2')
+    #print(res)
+    #res = compute('simplify', r'x + y - 3 + 2 + x')
+    #print(res)
+    #res = compute('calculate', r'\pi + \sin(\pi / 2)')
+    #print(res)
+    #res = compute('foo', 'bar')
+    #print(res)
+    #res = compute("solve y", "$y = 1 - 2 y^2$")
+    #print(res)
+    #res = compute("solve x", "$\\sin x = -1$")
+    #print(res)
+    #res = compute("solve x", "$\\sin x = 1/2$")
+    #print(res)
+
+    res = compute('simplify', r'$\begin{pmatrix} x \\ y \end{pmatrix} = \begin{pmatrix} 8 \\ -1 \end{pmatrix} + t \begin{pmatrix} 2 \\ 3 \end{pmatrix}$')
     print(res)
