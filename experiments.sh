e06e1cfb3d6a2de2705432128f6cbe8748e9bc1a
t-k- <clock126@126.com>
2023-08-02 14:23:34 -0400

    easier experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -1,6 +1,7 @@
 SETUP=${1}
 TOPIC=${2}
-FILTER=${3-None}
+DEVICES=${3-0}
+FILTER=${4-None}
 if [[ -z $SETUP ]]; then
     echo "please specify SETUP."
     exit 1
@@ -10,96 +11,81 @@ if [[ -z $TOPIC ]]; then
     exit 1
 fi
 
+export CUDA_VISIBLE_DEVICES=${DEVICES}
+
 detached_experiment() {
-    ID=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 10)
-    tmux new-session -c `pwd` -s batch_experiment_$ID -d
-    tmux send-keys -t batch_experiment_$ID "$1"
-    tmux send-keys -t batch_experiment_$ID Enter
+    ID=${1}-${2}
+    tmux new-session -c `pwd` -s $ID -d
+    tmux send-keys -t $ID "./experiments.sh $1 $2 $3"
+    tmux send-keys -t $ID Enter
 }
 
 case $SETUP in
-    test)
-    #./experiments.sh mh-chatgpt-2023-03-15 ${TOPIC} ${FILTER}
-
-    export CUDA_VISIBLE_DEVICES=7
+    manual)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=manual --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" --debug
     ;;
 
     batch)
-    detached_experiment './experiments.sh direct-vicuna-7b algebra'
-    detached_experiment './experiments.sh direct-vicuna-7b prealgebra'
-    detached_experiment './experiments.sh direct-vicuna-7b intermediate_algebra'
+    #detached_experiment direct-vicuna-7b precalculus 0
+    #detached_experiment cot-vicuna-7b precalculus 1
 
-    detached_experiment './experiments.sh cot-vicuna-7b algebra'
-    detached_experiment './experiments.sh cot-vicuna-7b prealgebra'
-    detached_experiment './experiments.sh cot-vicuna-7b intermediate_algebra'
+    #detached_experiment direct-vicuna-13b algebra 2
+    #detached_experiment cot-vicuna-13b algebra 3
 
-    #detached_experiment './experiments.sh direct-vicuna-7b precalculus'
-    #detached_experiment './experiments.sh cot-vicuna-7b    precalculus'
-    #detached_experiment './experiments.sh ia-vicuna-7b     precalculus'
-    #detached_experiment './experiments.sh direct-vicuna-13b precalculus'
-    #detached_experiment './experiments.sh cot-vicuna-13b    precalculus'
-    #detached_experiment './experiments.sh ia-vicuna-13b     precalculus'
-    #detached_experiment './experiments.sh direct-vicuna-33b precalculus'
-    #detached_experiment './experiments.sh cot-vicuna-33b    precalculus'
-    #detached_experiment './experiments.sh ia-vicuna-33b     precalculus'
+    #detached_experiment direct-vicuna-13b prealgebra 4
+    #detached_experiment cot-vicuna-13b prealgebra 5
 
-    #detached_experiment './experiments.sh mh-chatgpt-2023-03-15 precalculus'
+    #detached_experiment direct-vicuna-13b intermediate_algebra 6
+    #detached_experiment cot-vicuna-13b intermediate_algebra 7
+
+    detached_experiment direct-vicuna-13b precalculus 6
+    detached_experiment cot-vicuna-13b precalculus 7
     ;;
 
     direct-vicuna-7b)
-    export CUDA_VISIBLE_DEVICES=0
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
     ;;
 
     cot-vicuna-7b)
-    export CUDA_VISIBLE_DEVICES=0
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
     ;;
 
     ia-vicuna-7b)
-    export CUDA_VISIBLE_DEVICES=1
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
     ;;
 
     direct-vicuna-13b)
-    export CUDA_VISIBLE_DEVICES=2
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
     ;;
 
     cot-vicuna-13b)
-    export CUDA_VISIBLE_DEVICES=3
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
     ;;
 
     ia-vicuna-13b)
-    export CUDA_VISIBLE_DEVICES=4
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
     ;;
 
     direct-vicuna-33b)
-    export CUDA_VISIBLE_DEVICES=5,7
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=direct --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
     ;;
 
     cot-vicuna-33b)
-    export CUDA_VISIBLE_DEVICES=5,7
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=cot --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
     ;;
 
     ia-vicuna-33b)
-    export CUDA_VISIBLE_DEVICES=5,7
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=ia --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
@@ -131,19 +117,16 @@ case $SETUP in
     ;;
 
     example-vicuna-7b)
-    export CUDA_VISIBLE_DEVICES=0
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=example2 --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
     ;;
 
     example-vicuna-13b)
-    export CUDA_VISIBLE_DEVICES=5
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=example2 --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
     ;;
 
     example-vicuna-33b)
-    export CUDA_VISIBLE_DEVICES=3,7
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=example2 --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
@@ -160,7 +143,6 @@ case $SETUP in
     ;;
 
     mh-vicuna-7b)
-    export CUDA_VISIBLE_DEVICES=0
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
         --prompt_mode=mh --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
     ;;
