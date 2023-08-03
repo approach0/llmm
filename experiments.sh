0380e076c63969b1a8ea28a0acd9ad3bd078a789
t-k- <clock126@126.com>
2023-08-03 14:16:51 -0400

    added metric argument to experiment.sh

--- experiments.sh
+++ experiments.sh
@@ -3,6 +3,9 @@ TOPIC=${2}
 DEVICES=${3-0}
 FILTER=${4-None} # 235.json, 439.json
 # could be found with larger index: 218.json
+
+METRIC=maj@3
+
 if [[ -z $SETUP ]]; then
     echo "please specify SETUP."
     exit 1
@@ -31,123 +34,120 @@ case $SETUP in
     ;;
 
     batch)
-    #detached_experiment direct-vicuna-7b precalculus 0
-    #detached_experiment cot-vicuna-7b precalculus 1
+    detached_experiment direct-vicuna-7b precalculus 0
+    detached_experiment cot-vicuna-7b precalculus 0
 
-    #detached_experiment direct-vicuna-13b algebra 2
-    #detached_experiment cot-vicuna-13b algebra 3
+    detached_experiment direct-vicuna-7b algebra 2
+    detached_experiment cot-vicuna-7b algebra 2
 
-    #detached_experiment direct-vicuna-13b prealgebra 4
-    #detached_experiment cot-vicuna-13b prealgebra 5
+    detached_experiment direct-vicuna-7b prealgebra 3
+    detached_experiment cot-vicuna-7b prealgebra 3
 
-    #detached_experiment direct-vicuna-13b intermediate_algebra 6
-    #detached_experiment cot-vicuna-13b intermediate_algebra 7
-
-    detached_experiment direct-vicuna-13b precalculus 6
-    detached_experiment cot-vicuna-13b precalculus 7
+    detached_experiment direct-vicuna-7b intermediate_algebra 5
+    detached_experiment cot-vicuna-7b intermediate_algebra 5
     ;;
 
     direct-vicuna-7b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+        --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" \
+        --metric $METRIC
     ;;
 
     cot-vicuna-7b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+        --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" \
+        --metric $METRIC
     ;;
 
     ia-vicuna-7b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+        --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" \
+        --metric $METRIC
     ;;
 
     direct-vicuna-13b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
+        --prompt_mode=direct --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']" \
+        --metric $METRIC
     ;;
 
     cot-vicuna-13b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
+        --prompt_mode=cot --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']" \
+        --metric $METRIC
     ;;
 
     ia-vicuna-13b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
+        --prompt_mode=ia --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']" \
+        --metric $METRIC
     ;;
 
     direct-vicuna-33b)
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=direct --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
+        --prompt_mode=direct --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']" \
+        --metric $METRIC
     ;;
 
     cot-vicuna-33b)
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=cot --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
+        --prompt_mode=cot --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']" \
+        --metric $METRIC
     ;;
 
     ia-vicuna-33b)
     export TRANSFORMERS_CACHE='./cache'
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=ia --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
+        --prompt_mode=ia --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']" \
+        --metric $METRIC
     ;;
 
     direct-chatgpt-2023-03-15)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=direct --run_pass=chatgpt --args="[]"
+        --prompt_mode=direct --run_pass=chatgpt --args="[]" --metric $METRIC
     ;;
 
     cot-chatgpt-2023-03-15)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=cot --run_pass=chatgpt --args="[]"
+        --prompt_mode=cot --run_pass=chatgpt --args="[]" --metric $METRIC
     ;;
 
     ia-chatgpt-2023-03-15)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=ia --run_pass=chatgpt --args="[]"
+        --prompt_mode=ia --run_pass=chatgpt --args="[]" --metric $METRIC
     ;;
 
     cot-gpt4-2023-july)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=cot --run_pass=gpt4 --args="[]"
+        --prompt_mode=cot --run_pass=gpt4 --args="[]" --metric $METRIC
     ;;
 
     ia-gpt4-2023-july)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=ia --run_pass=gpt4 --args="[]"
+        --prompt_mode=ia --run_pass=gpt4 --args="[]" --metric $METRIC
     ;;
 
     example-vicuna-7b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=example2 --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
-    ;;
-
-    example-vicuna-13b)
-    python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=example2 --run_pass=vicuna --args="[1,'lmsys/vicuna-13b-v1.3']"
-    ;;
-
-    example-vicuna-33b)
-    export TRANSFORMERS_CACHE='./cache'
-    python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=example2 --run_pass=vicuna --args="[2,'lmsys/vicuna-33b-v1.3']"
+        --prompt_mode=example2 --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" \
+        --metric $METRIC
     ;;
 
     example-chatgpt-2023-03-15)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=example2 --run_pass=chatgpt --args="[]"
+        --prompt_mode=example2 --run_pass=chatgpt --args="[]" --metric $METRIC
     ;;
 
     mh-chatgpt-2023-03-15)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=mh --run_pass=chatgpt --args="[]" --skip_existing True
+        --prompt_mode=mh --run_pass=chatgpt --args="[]" --skip_existing True --metric $METRIC
     ;;
 
     mh-vicuna-7b)
     python tools/multi-hops.py --logname $SETUP --topic $TOPIC --fname_filter $FILTER \
-        --prompt_mode=mh --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']"
+        --prompt_mode=mh --run_pass=vicuna --args="[1,'lmsys/vicuna-7b-v1.3']" \
+        --metric $METRIC
     ;;
 esac
