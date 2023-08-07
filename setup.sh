6d10493896bffd8c3dbd209bce3a02de6648d943
t-k- <clock126@126.com>
2023-08-07 02:42:34 -0400

    working setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-conda create -n llmm python=3.10
+conda create -y -n llmm python=3.10
 conda activate llmm
 
-pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu116
-conda install -c "nvidia/label/cuda-11.6.2" cuda-toolkit
+pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu118
 pip3 install deepspeed==0.10.0
+conda install -y -c "nvidia/label/cuda-11.8.0" cuda-toolkit
 
 python3 ./test-setup.py
 
-pip3 install transformers==4.30.2
+pip3 install transformers
 pip3 install sentencepiece
 pip3 install peft
-conda install -c conda-forge gxx_linux-64=11.2.0
+pip install -r pya0/requirements.txt
