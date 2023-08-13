db665dbcb61a6e12a67187584f78fa60f41edf87
t-k- <clock126@126.com>
2023-08-12 21:56:46 -0400

    utils2.sh: added copy_for_publish()

--- utils2.sh
+++ utils2.sh
@@ -7,8 +7,22 @@ export NCCL_P2P_DISABLE=1 # direct access between GPUs? using NVLink or PCI.
 #export TORCH_DISTRIBUTED_DEBUG=DETAIL
 export TORCH_DISTRIBUTED_DEBUG=OFF
 
+copy_for_publish() {
+    cp $1/config.json $2
+    cp $1/generation_config.json $2
+    cp $1/pytorch_model-*.bin $2
+    cp $1/pytorch_model.bin.index.json $2
+
+    cp $1/special_tokens_map.json $2
+    cp $1/tokenizer_config.json $2
+    cp $1/tokenizer.model $2
+}
+copy_for_publish output/13B-mathy-FFT ../azbert/ckpt/
+
 export CUDA_VISIBLE_DEVICES=4
 python utils2.py test \
-    --cache_dir ./data \
-    --model_path WizardLM/WizardMath-13B-V1.0 \
+    --model_path ../azbert/ckpt/ \
     'Solve $x^2 = 4$.'
+
+    #--cache_dir ./data \
+    #--model_path WizardLM/WizardMath-13B-V1.0 \
