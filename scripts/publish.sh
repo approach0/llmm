63b4a8832742fcca73c4612a2694640e5ed92ab4
w32zhong <clock126@126.com>
2023-08-26 03:52:59 -0400

    fix scripts/publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,15 @@ copy_for_publish() {
     cp $1/pytorch_model-*.bin $2
     cp $1/pytorch_model.bin.index.json $2
 
+    cp $1/adapter_config.json $2
+    cp $1/adapter_model.bin $2
+
     cp $1/special_tokens_map.json $2
     cp $1/tokenizer_config.json $2
     cp $1/tokenizer.model $2
+    cp $1/tokenizer.json $2
 
     cp $1/*.ini $2
 }
 
-copy_for_publish output/finetune_phase2__mathy_fft ../azbert/ckpt/
+copy_for_publish output/finetune_phase2__mathy_lora.old ../azbert-lora/ckpt/
