2a0bb13fc1e5f7a04de4f637370c5aab315b327a
w32zhong <clock126@126.com>
2023-08-26 02:43:31 -0400

    added scripts/publish.sh

---
+++ scripts/publish.sh
@@ -0,0 +1,14 @@
+copy_for_publish() {
+    cp $1/config.json $2
+    cp $1/generation_config.json $2
+    cp $1/pytorch_model-*.bin $2
+    cp $1/pytorch_model.bin.index.json $2
+
+    cp $1/special_tokens_map.json $2
+    cp $1/tokenizer_config.json $2
+    cp $1/tokenizer.model $2
+
+    cp $1/*.ini $2
+}
+
+copy_for_publish output/finetune_phase2__mathy_fft ../azbert/ckpt/
