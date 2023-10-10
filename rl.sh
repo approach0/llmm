71c3e83ced5f3efda4d9fcb7f953428ba47e3395
w32zhong <clock126@126.com>
2023-10-10 23:56:31 +0000

    update rl.sh

--- rl.sh
+++ rl.sh
@@ -96,44 +96,44 @@ case $1 in
     ;;
 
     batch_infer_w_16v100s)
-        experiment=inference__mammoth
+        experiment=inference_baseline_using_vllm
         model=TIGER-Lab/MAmmoTH-13B
         run_uid=13b_mammoth
 
         export CUDA_VISIBLE_DEVICES=0
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset  0 --data_cutoff 35"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset  0 --data_cutoff 35"
         export CUDA_VISIBLE_DEVICES=1
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 35 --data_cutoff 70"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 35 --data_cutoff 70"
         export CUDA_VISIBLE_DEVICES=2
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 105 --data_cutoff 140"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 105 --data_cutoff 140"
         export CUDA_VISIBLE_DEVICES=3
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 140 --data_cutoff 175"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 140 --data_cutoff 175"
 
         export CUDA_VISIBLE_DEVICES=4
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 175 --data_cutoff 210"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 175 --data_cutoff 210"
         export CUDA_VISIBLE_DEVICES=5
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 210 --data_cutoff 245"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 210 --data_cutoff 245"
         export CUDA_VISIBLE_DEVICES=6
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 245 --data_cutoff 280"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 245 --data_cutoff 280"
         export CUDA_VISIBLE_DEVICES=7
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 280 --data_cutoff 315"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 280 --data_cutoff 315"
 
         export CUDA_VISIBLE_DEVICES=8
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 315 --data_cutoff 350"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 315 --data_cutoff 350"
         export CUDA_VISIBLE_DEVICES=9
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 350 --data_cutoff 385"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 350 --data_cutoff 385"
         export CUDA_VISIBLE_DEVICES=10
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 385 --data_cutoff 420"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 385 --data_cutoff 420"
         export CUDA_VISIBLE_DEVICES=11
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 420 --data_cutoff 455"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 420 --data_cutoff 455"
 
         export CUDA_VISIBLE_DEVICES=12
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 455 --data_cutoff 490"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 455 --data_cutoff 490"
         export CUDA_VISIBLE_DEVICES=13
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 490 --data_cutoff 525"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 490 --data_cutoff 525"
         export CUDA_VISIBLE_DEVICES=14
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 525 --data_cutoff 560"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 525 --data_cutoff 560"
         export CUDA_VISIBLE_DEVICES=15
-        detached_rl $experiment "--run_uid $run_uid --model $model --data_offset 70 --data_cutoff 105"
+        detached_rl $experiment "--run_uid $run_uid --model $model --tokenizer $model --data_offset 70 --data_cutoff 105"
     ;;
 esac
