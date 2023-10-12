2d7038e90a2478844d93c8035d384ee975187abc
t-k- <clock126@126.com>
2023-10-12 17:34:00 -0400

    update scripts/publish-data.sh

--- scripts/publish-data.sh
+++ scripts/publish-data.sh
@@ -1,8 +1,10 @@
 rm -rf ~/.cache/huggingface/datasets
 rm -rf output/final-*
+rm -rf output/retrieval-augment-finetune/
 python tools/data-gen-final-dataset.py output/mcts_explore_fulltopics_using_chatgpt/ --minrel 1 -o output/final-dataset.json
 #python tools/data-gen-final-dataset.py output/mcts_explore_trees_using_chatgpt/collection/ --minrel 2 -o output/final-dataset-extra.json
 #python tools/data-gen-merged-pairs.py merge output/final-*.json -o output/final-merged.json
 cp output/final-dataset.json output/final-merged.json
 python tools/data-gen-merged-pairs.py push approach0/retrieval-augment-finetune --train_path output/final-merged.json
-python tools/inspect_output.py final_dataset approach0/retrieval-augment-finetune -o output/final-dataset
+python tools/inspect_output.py final_dataset approach0/retrieval-augment-finetune -o output
+sh scripts/sync.sh
