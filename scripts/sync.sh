b332090f7bdba348f612639340b13290069a06f2
t-k- <clock126@126.com>
2023-10-19 18:36:36 -0400

    minor save.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,5 @@
 # export RSYNC_PASSWORD=takemymoney
 while true; do
-	rsync -zav --delete ./output/retrieval-augment-finetune rsync://rsyncclient@approach0.xyz/data/GCR
+	rsync -zav --delete ./output/final_inference/* rsync://rsyncclient@approach0.xyz/data/basilisk
 	sleep 3
 done
